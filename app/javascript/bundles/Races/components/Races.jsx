81da3132f853e35de59356e013ba6fe28803465b
Jah Seng Lee <jahseng.lee@gmail.com>
2025-02-17 15:21:11 +1300

    Add basic navbar

--- app/javascript/bundles/Races/components/Races.jsx
+++ app/javascript/bundles/Races/components/Races.jsx
@@ -2,23 +2,26 @@ import React, { useState } from 'react';
 
 import * as style from './Races.module.css';
 
+import AddIcon from '@mui/icons-material/Add';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
 
 const Races = (props) => {
-  const [name, setName] = useState(props.name);
-
   return (
-    <div>
-      <Button variant="contained">Hello world</Button>;
-      <h3>Hello, {name}!</h3>
-      <hr />
-      <form>
-        <label className={style.bright} htmlFor="name">
-          Say hello to:
-          <input id="name" type="text" value={name} onChange={(e) => setName(e.target.value)} />
-        </label>
-      </form>
-    </div>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            LiveHeatsCodingChallenge
+          </Typography>
+          <Button color="inherit" startIcon={<AddIcon />}>Create race</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 };
 
