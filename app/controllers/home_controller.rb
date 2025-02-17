29c3b71c5e0d612432b4bfbdf95dbcff450cd76b
Jah Seng Lee <jahseng.lee@gmail.com>
2025-02-17 15:07:16 +1300

    Rename just the controller/routes to Home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
-class RacesController < ApplicationController
+class HomeController < ApplicationController
   def index; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root "races#index"
+  root "home#index"
 
   get "up" => "rails/health#show", as: :rails_health_check
 end
